1959d29c186232e660833f1365eaba9d01eef0a6
XingGuoZM <18969176324@163.com>
2023-07-05 17:01:48 +0800

    react developer blog

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+// const htmlWebpackPlugin = require('html-webpack-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.tsx',
+    vendor: ['react', 'react-dom']
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '/public'),
+    compress: true,
+    host: '0.0.0.0',
+    port: 8000
+  },
+  devtool: 'source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: { presets: ["@babel/preset-env"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [],
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"]
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: __dirname + '/public'
+  }
+}
