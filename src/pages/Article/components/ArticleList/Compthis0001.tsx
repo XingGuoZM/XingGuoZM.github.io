994e8bc04cb29d44b5b2d93d47ff28ef23a61b1d
XingGuoZM <18969176324@163.com>
2023-07-13 20:19:56 +0800

    pc端居中

--- src/pages/Article/components/ArticleList/Compthis0001.tsx
+++ src/pages/Article/components/ArticleList/Compthis0001.tsx
@@ -1,16 +1,15 @@
 
-  import React,{useEffect} from 'react';
- 
-  import 'highlight.js/styles/xcode.css';
-  const hljs = require('highlight.js');
+import React, { useEffect } from 'react';
 
-  export default () => {
-    useEffect(()=>{
-      document.querySelectorAll('code').forEach(el => {
-        el.style.backgroundColor='#f8f8f8';
-        hljs.highlightElement(el);
-      });
-    },[])
-    return <div style={{fontSize:'0.26rem'}} dangerouslySetInnerHTML={{__html:``}}></div>
-  }
-  
+import 'highlight.js/styles/xcode.css';
+const hljs = require('highlight.js');
+
+export default () => {
+  useEffect(() => {
+    document.querySelectorAll('code').forEach(el => {
+      el.style.backgroundColor = '#f8f8f8';
+      hljs.highlightElement(el);
+    });
+  }, [])
+  return <div style={{ fontSize: '0.26rem' }} dangerouslySetInnerHTML={{ __html: `` }}></div>
+}
