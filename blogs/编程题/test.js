74ee01fd593f44caf5927c5e8b5c2a53ebca35d0
XingGuoZM <18969176324@163.com>
2023-09-05 12:11:00 +0800

    编程题

--- blogs/编程题/test.js
+++ blogs/编程题/test.js
@@ -1,65 +1,40 @@
-// 分别封装精确运算的加减乘除四个方法
 
-String.prototype.numberAdd = function (number) {
-
-  const Max = Math.max(this.length, number.length);
-  let curry = 0;
-  let n1 = this.padStart(Max, '0');
-  let n2 = number.padStart(Max, '0');
-  let sum = '';
-  for (let i = Max - 1; i >= 0; i--) {
-    const temp = Number(n1[i]) + Number(n2[i]) + curry;
-    sum = temp % 10 + sum;
-    curry = temp > 9 ? 1 : 0;
-  }
-  return curry === 1 ? '1' + sum : sum;
-}
-String.prototype.numberMinus = function (number) {
-  const Max = Math.max(this.length, number.length);
-  let curry = 0;
-
-  let flag = true;
-  let n1 = this.padStart(Max, '0');
-  let n2 = number.padStart(Max, '0');
-  if (Number(this) <= (number)) {
-    n1 = number.padStart(Max, '0');
-    n2 = this.padStart(Max, '0');
-    flag = false;
-  }
-  let ans = ''
-  for (let i = Max - 1; i >= 0; i--) {
-    let num1 = Number(n1[i]), num2 = Number(n2[i])
-    curry === 1 && num1--;
-    curry = num1 < num2 ? 1 : 0;
-    const temp = num1 + 10 * curry - num2;
-
-    ans = temp + ans;
-  }
-  return flag ? ans : `-${Number(ans)}`;
-}
-String.prototype.numberMultiply = function (number) {
-  const n1 = Number(this);
-  const n2 = Number(number);
-  const min = n1 < n2 ? n1 : n2;
-  const max = n1 < n2 ? number : this;
-  let sum = '';
-  for (let i = 0; i < min; i++) {
-    sum = sum.numberAdd(max)
-  }
-  return sum;
-}
-String.prototype.numberDivide = function (number) {
-  let t1 = 0
-  let t2 = 0
-  let r1
-  let r2
-  try {
-    t1 = this.split('.')[1].length
-    t2 = number.split('.')[1].length
-  } catch (e) { }
-  r1 = Number(this.replace('.', ''))
-  r2 = Number(number.replace('.', ''))
-  return (r1 / r2) * Math.pow(10, t2 - t1)
+/**
+ *
+ */
+// 4位读取
+function translate (str) {
+  const unit = ['千', '百', '十', ''];
+  const UpperNumber = '零一二三四五六七八九'
+  return str.padStart(4, '0').split('').reduce((acc, item, index) => {
+    if (item === '0') {
+      if (acc === '' || acc[acc.length - 1] === UpperNumber[0]) return acc;
+      return `${acc}${UpperNumber[0]}`;
+    }
+    return acc += UpperNumber[item] + unit[index % 4];
+  }, '');
 }
 
-console.log('2'.numberDivide('3'));
+function split (str) {
+  const arr = [];
+  let temp = ''
+  for (let i = str.length - 1; i >= 0; i--) {
+    temp = str[i] + temp;
+    if (temp.length === 4) {
+      arr.push(temp);
+      temp = '';
+    }
+  }
+  temp.length > 0 && arr.push(temp)
+  return arr;
+}
+
+function convert2Chinese (num) {
+  const unit = ['', '万', '亿', '兆']
+  const arr = split(String(num));
+  return arr.reduceRight((acc, item, index) => {
+    return acc += translate(item) + unit[index % 4];
+  }, '');
+}
+
+console.log(convert2Chinese(1005))
