d3d41607f774941db6258ef03054ee4321bddf10
frameproli <frameproli@tencent.com>
2023-11-29 13:13:13 +0800

    Revert "mobile"
    This reverts commit c623e3186eca7c307db505356e76fea0aba458c7.

--- blogs/编程题/test.js
+++ blogs/编程题/test.js
@@ -1,37 +1,16 @@
 // float
 
-// function parseFloat (value, n) {
-//   const [a, b] = String(value).split('.')
-//   if (b === void 0)
-//     return value + '.'.padEnd(n + 1, 0);
-//   if (b.length <= n)
-//     return a + '.' + b.padEnd(n, 0);
+function parseFloat (value, n) {
+  const [a, b] = String(value).split('.')
+  if (b === void 0)
+    return value + '.'.padEnd(n + 1, 0);
+  if (b.length <= n)
+    return a + '.' + b.padEnd(n, 0);
 
-//   const pre = Number(b[n - 1]);
-//   const end = Number(b[n]);
-//   const curry = end >= 5 ? (pre + 1) : pre;
-//   return a + '.' + b.slice(0, n - 1) + curry;
-// }
-
-// console.log(parseFloat(2.12, 4));
-
-function sum(...args){
-  if(args.length===0) return 0;
-  let arr = [].concat(args)
-  return function fn(){
-    if(arguments.length===0){
-      return arr.reduce((cur,acc)=>cur+acc);
-    }
-    arr = arr.concat([...arguments]);
-    return fn;
-  }
+  const pre = Number(b[n - 1]);
+  const end = Number(b[n]);
+  const curry = end >= 5 ? (pre + 1) : pre;
+  return a + '.' + b.slice(0, n - 1) + curry;
 }
-// sum() == 0
-// sum(1)(2)(3)()
-// sum(3)(4)() == 7
-// sum(v1)(v2)...(vn)() == v1+v2+...+vn
 
-
-console.log(sum())
-console.log(sum(1)(2)(3)())
-console.log(sum(3)(4)())
+console.log(parseFloat(2.12, 4));
